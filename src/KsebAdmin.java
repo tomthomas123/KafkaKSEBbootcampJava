879a26a0f600c745148a29724b086431f7ed17ed
tomthomas123 <tomthomas1248@gmail.com>
2022-11-11 11:25:20 +0530

    choice 7 view all bill addded

--- src/KsebAdmin.java
+++ src/KsebAdmin.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.util.Date;
 import java.time.Month;
@@ -160,11 +162,93 @@ public class KsebAdmin {
 
 
                 case 6:
-                    System.out.println("Generate Bill selected");
+                    System.out.println("Selected bill generation");
+                    GregorianCalendar date = new GregorianCalendar();
+                    int cMonth = date.get(Calendar.MONTH);
+                    int cYear = date.get(Calendar.YEAR);
+                    cMonth = cMonth+1;
+                    System.out.println("Current month is  " + cMonth);
+                    System.out.println("Current year is  " + cYear);
+                    System.out.println("Bill Generated succesfully");
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb", "root", "");
+                        String sql = "DELETE FROM `bill` WHERE `month`= '" + cMonth + "'AND `year`= '" + cYear + "'";
+                        Statement stmt = con.createStatement();
+                        stmt.executeUpdate(sql);
+                        String sql1 = "SELECT `id` FROM `consumer` ";
+                        Statement stmt1 = con.createStatement();
+                        ResultSet rs = stmt1.executeQuery(sql1);
+                        while (rs.next())
+                        {
+                            int id = rs.getInt("id");
+                            String sql2 = "select SUM(`unit`) from usages where month(date) = '"+cMonth+"' AND year(date) = '"+cYear+"' AND `user_id` ='"+id+"'";
+                            Statement stmt2 = con.createStatement();
+                            ResultSet rs1 = stmt2.executeQuery(sql2);
+                            while (rs1.next())
+                            {
+                                int add = rs1.getInt("SUM(`unit`)");
+                                int status = 0;
+                                int totalBill = add * 5;
+                                int min = 10000;
+                                int max = 99999;
+                                int invoice = (int)(Math.random() * (max - min + 1) + min);
+                                String sql3 = "INSERT INTO `bill`(`consumer_id`, `month`, `year`, `bill`, `paid_status`, `billdate`, `total_units`, `duedate`, `invoice`) VALUES (?,?,?,?,?,now(),?,now()+ interval 14 day,?)";
+                                PreparedStatement stmt3 = con.prepareStatement(sql3);
+                                stmt3.setInt(1, id);
+                                stmt3.setInt(2, cMonth);
+                                stmt3.setInt(3, cYear);
+                                stmt3.setInt(4, totalBill);
+                                stmt3.setInt(5, 0);
+                                stmt3.setInt(6, add);
+                                stmt3.setInt(7,invoice );
+                                stmt3.executeUpdate();
+                            }
+                        }
 
-                                    break;
+                    }
+                    catch(Exception e){
+                        System.out.println(e);
+                    }
+                    break;
                                     case 7:
                                         System.out.println("View Bill");
+
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb","root","");
+                        String sql = "SELECT  b.`consumer_id`, b.`month`, b.`year`, b.`bill`, b.`paid_status`, b.`billdate`, b.`total_units`, b.`duedate`, b.`invoice`,c.consumer_id,c.consumer_name FROM `bill` b JOIN consumer c ON b.consumer_id=c.id";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()){
+                            String getConsumerId = rs.getString("consumer_id");
+                            String getMonth= rs.getString("month");
+                            String getYear = rs.getString("year");
+                            String getBill = rs.getString("bill");
+                            String getPaidStatus = rs.getString("paid_status");
+                            String getbilldate = rs.getString("billdate");
+                            String getTotalUnit = rs.getString("total_units");
+                            String getDueDate = rs.getString("duedate");
+                            String getInvoice = rs.getString("invoice");
+                            String getConsumerCode = rs.getString("consumer_id");
+                            String getConsumerName = rs.getString("consumer_name");
+                            System.out.println("Consumer Id="+getConsumerId);
+                            System.out.println("Month="+getMonth);
+                            System.out.println("Year="+getYear);
+                            System.out.println("Bill ="+getBill);
+                            System.out.println("PaidStatus ="+getPaidStatus);
+                            System.out.println("Bill Date ="+getbilldate);
+                            System.out.println("Total Unit ="+getTotalUnit);
+                            System.out.println("Due Date ="+getDueDate);
+                            System.out.println("Invoice ="+getInvoice);
+                            System.out.println("Consumer Code ="+getConsumerCode);
+                            System.out.println("Consumer Name="+getConsumerName+"\n");
+                        }
+
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
 
                                         break;
                                     case 8:
